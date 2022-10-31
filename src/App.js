b0b9393415f54667d4172a054a1dfca184a386ff
augustolumes <82983177+augustolumes@users.noreply.github.com>
2022-10-31 06:43:23 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Calculator from './components/Calculator';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 style={{margin: "25px auto"}}>Calculadora</h1>
+      <Calculator/>
+    </div>
+  );
+}
+
+export default App;
